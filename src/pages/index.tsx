2738c1508d08c1a2f4485e94fe274256df25222f
Fernando Bosco <fbosco@dlocal.com>
2022-04-22 22:47:44 -0300

    add dark switcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { Box, Center } from "@chakra-ui/react";
 import React from "react";
 import ChakraTable from "../components/ChakraTable";
+import { DarkModeSwitch } from "../components/DarkModeSwitch";
 
 export default function HomePage() {
   return (
     <>
       <Box h="100vh">
+        <DarkModeSwitch />
         <Center h="100%">
           <Box shadow="md">
             <ChakraTable />
