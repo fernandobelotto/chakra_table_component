0dc27dbdac01f87adebb9a515abbe1b644285c50
Fernando Bosco <fbosco@dlocal.com>
2022-04-22 22:46:43 -0300

    prettier

--- src/components/ChakraTable.tsx
+++ src/components/ChakraTable.tsx
@@ -1,90 +1,90 @@
-import { TriangleDownIcon, TriangleUpIcon } from "@chakra-ui/icons"
-import { Table, Thead, Tr, Th, chakra, Tbody, Td } from "@chakra-ui/react"
-import React, { useMemo } from "react"
-import {useTable, useSortBy} from 'react-table'
+import { TriangleDownIcon, TriangleUpIcon } from "@chakra-ui/icons";
+import { Table, Thead, Tr, Th, chakra, Tbody, Td } from "@chakra-ui/react";
+import React, { useMemo } from "react";
+import { useTable, useSortBy } from "react-table";
 export default function ChakraTable() {
-    const data = useMemo(
-      () => [
-        {
-          fromUnit: 'inches',
-          toUnit: 'millimetres (mm)',
-          factor: 25.4,
-        },
-        {
-          fromUnit: 'feet',
-          toUnit: 'centimetres (cm)',
-          factor: 30.48,
-        },
-        {
-          fromUnit: 'yards',
-          toUnit: 'metres (m)',
-          factor: 0.91444,
-        },
-      ],
-      [],
-    )
+  const data = useMemo(
+    () => [
+      {
+        fromUnit: "inches",
+        toUnit: "millimetres (mm)",
+        factor: 25.4,
+      },
+      {
+        fromUnit: "feet",
+        toUnit: "centimetres (cm)",
+        factor: 30.48,
+      },
+      {
+        fromUnit: "yards",
+        toUnit: "metres (m)",
+        factor: 0.91444,
+      },
+    ],
+    []
+  );
 
-    const columns = useMemo(
-      () => [
-        {
-          Header: 'To convert',
-          accessor: 'fromUnit',
-        },
-        {
-          Header: 'Into',
-          accessor: 'toUnit',
-        },
-        {
-          Header: 'Multiply by',
-          accessor: 'factor',
-          isNumeric: true,
-        },
-      ],
-      [],
-    )
+  const columns = useMemo(
+    () => [
+      {
+        Header: "To convert",
+        accessor: "fromUnit",
+      },
+      {
+        Header: "Into",
+        accessor: "toUnit",
+      },
+      {
+        Header: "Multiply by",
+        accessor: "factor",
+        isNumeric: true,
+      },
+    ],
+    []
+  );
 
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-      useTable({ columns, data }, useSortBy)
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data }, useSortBy);
 
-    return (
-      <Table {...getTableProps()}>
-        <Thead>
-          {headerGroups.map((headerGroup) => (
-            <Tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <Th
-                  {...column.getHeaderProps(column.getSortByToggleProps())}
-                  isNumeric={column.isNumeric}
-                >
-                  {column.render('Header')}
-                  <chakra.span pl='4'>
-                    {column.isSorted ? (
-                      column.isSortedDesc ? (
-                        <TriangleDownIcon aria-label='sorted descending' />
-                      ) : (
-                        <TriangleUpIcon aria-label='sorted ascending' />
-                      )
-                    ) : null}
-                  </chakra.span>
-                </Th>
+  return (
+    <Table {...getTableProps()}>
+      <Thead>
+        {headerGroups.map((headerGroup) => (
+          <Tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <Th
+                {...column.getHeaderProps(column.getSortByToggleProps())}
+                isNumeric={column.isNumeric}
+              >
+                {column.render("Header")}
+                <chakra.span pl="4">
+                  {column.isSorted ? (
+                    column.isSortedDesc ? (
+                      <TriangleDownIcon aria-label="sorted descending" />
+                    ) : (
+                      <TriangleUpIcon aria-label="sorted ascending" />
+                    )
+                  ) : null}
+                </chakra.span>
+              </Th>
+            ))}
+          </Tr>
+        ))}
+      </Thead>
+      <Tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <Tr {...row.getRowProps()}>
+              {row.cells.map((cell) => (
+                <Td {...cell.getCellProps()} isNumeric={cell.column.isNumeric}>
+                  {cell.render("Cell")}
+                </Td>
               ))}
             </Tr>
-          ))}
-        </Thead>
-        <Tbody {...getTableBodyProps()}>
-          {rows.map((row) => {
-            prepareRow(row)
-            return (
-              <Tr {...row.getRowProps()}>
-                {row.cells.map((cell) => (
-                  <Td {...cell.getCellProps()} isNumeric={cell.column.isNumeric}>
-                    {cell.render('Cell')}
-                  </Td>
-                ))}
-              </Tr>
-            )
-          })}
-        </Tbody>
-      </Table>
-    )
-  }
+          );
+        })}
+      </Tbody>
+    </Table>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,17 @@
-import { Box, Center } from '@chakra-ui/react'
-import React from 'react'
-import ChakraTable from '../components/ChakraTable'
+import { Box, Center } from "@chakra-ui/react";
+import React from "react";
+import ChakraTable from "../components/ChakraTable";
 
 export default function HomePage() {
   return (
     <>
-      <Box h='100vh'>
-        <Center h='100%'>
-          <Box shadow='md'>
-
-          <ChakraTable />
+      <Box h="100vh">
+        <Center h="100%">
+          <Box shadow="md">
+            <ChakraTable />
           </Box>
         </Center>
       </Box>
     </>
-  )
+  );
 }
